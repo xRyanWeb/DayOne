5b9d72ad2a42e0637f66e6cf3e5bcc1a82cf51d2
Ryan JG <ryangregoire286@gmail.com>
2024-12-02 12:05:35 -0500

    Added Code

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final int maxHours = 11;
+        final int maxWage = 15;
+        final int anotherVar = maxWage * maxHours;
+        final int anotherValued = 2 + 15;
+
+        System.out.println(anotherVar);
+
+        int x = 3;
+        System.out.println("x: " + x);
+        int y = ((x * 5) / 5) ;
+
+        x += x + 5;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        float f = 1.0f;
+        double d = 1.0d;
+        byte b = 23;
+        short s = 12;
+        long l = 13;
+
+        
+
+
+
+
+
+    }
+}
+
+/*
+ + Add
+ - Sub
+ * Mul
+ / Div
+ --
+ ++
+ +=
+ -=
+ *=
+ /=
+
+* */
